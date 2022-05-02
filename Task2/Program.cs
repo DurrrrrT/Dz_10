ade22746976ae0cb0b8ff84dbc98dfd4a39739e8
Amir <amir200320032003@gmail.com>
2022-05-02 19:41:26 +0300

    first commit

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+//  в промежутке от M до N. M = 1; N = 15 -> 120 M = 4; N = 8. -> 30
+
+Console.WriteLine("Задайте число M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int NaturalNum(int m, int n)
+{
+    if (m < n)
+    {
+        sum += m;
+        NaturalNum(m + 1, n);
+        return sum + n;
+    }
+    else if (m > n)
+    {
+        sum += n;
+        NaturalNum(m, n + 1);
+        return sum + m;
+    }
+    else return 0;
+}
+Console.WriteLine($"Сумма элементов равна: {NaturalNum(m,n)} ");
---
+++ Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N в обратном порядке. 
+
+Console.WriteLine("Задайте число M ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте число M ");
+int N = Convert.ToInt32(Console.ReadLine());
+Recursion(M,N);
+void Recursion(int m, int n)
+{
+    if (m == n) return;
+    Recursion(m - 1, n);
+}
+
+//Вариант 2.
+
+// Console.WriteLine("Задайте число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Recursion(number);
+// void Recursion(int n)
+// {
+//     if (n == 0) return;
+//     Console.Write(n + " ");
+//     Recursion(n - 1);
+// }
+
+
+
